f0315f60fa01c70a9bd2c3c7172daf458a2d5c8e
gustav <gustav@bernpaintner.com>
2020-11-13 21:00:38 +0100

    Created hello world app with kivy!!

--- stop_searcher/kivy_view.py
+++ stop_searcher/kivy_view.py
@@ -1,1 +1,12 @@
-# finally transparent backgrounds...
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World!')
+
+
+if __name__ == '__main__':
+    TestApp().run()
+
