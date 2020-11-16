f60094795fa6352b00408338c4b3a0e38a8010bc
gustav <gustav@bernpaintner.com>
2020-11-16 21:25:58 +0100

    Created rought draft of Table widget

--- stop_searcher/kivy_view.py
+++ stop_searcher/kivy_view.py
@@ -1,12 +1,41 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
 
 
-class TestApp(App):
+class Table(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._entries = kwargs.get('entries', None)
+        self._entry_map = kwargs.get('entry_map', None)
+
+
+    @property
+    def entries(self):
+        return self._entries
+
+
+    @entries.setter
+    def entries(self, e):
+        self._entries = e
+        for entry in e:
+            for col in range(self.cols):
+                self.add_widget(self._entry_map[col](entry))
+
+
+    @entry_map.setter
+    def entry_map(self, em):
+        self._entry_map = em
+        self.cols = len(em)
+
+
+class MainApp(App):
     def build(self):
-        return Button(text='Hello World!')
+        t = Table()
+        return Table()
 
 
 if __name__ == '__main__':
-    TestApp().run()
+    MainApp().run()
 
