7cfc8815a688948d785cd10f13327f08e07e7bf9
gustav <gustav@bernpaintner.com>
2020-11-19 18:46:48 +0100

    Finished Table class

--- stop_searcher/kivy_view.py
+++ stop_searcher/kivy_view.py
@@ -1,39 +1,62 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from stop_controller import get_departures_from
 
 
 class Table(GridLayout):
     def __init__(self, **kwargs):
+        self._source = kwargs.pop('source', None)
+        self._entry_maps = kwargs.pop('entry_maps', None)
         super().__init__(**kwargs)
-        self._entries = kwargs.get('entries', None)
-        self._entry_map = kwargs.get('entry_map', None)
 
 
+    def update(self):
+        try:
+            self._entries = self._source()
+        except TypeError as e:
+            self._entries = self._source
+        self.cols = len(self._entry_maps)
+        # Remove old children.
+        self.clear_widgets()
+        # Add new children.
+        for entry in self._entries:
+            for entry_map in self._entry_maps:
+                self.add_widget(entry_map(entry))
+                print(f'adding {entry} as {entry_map(entry)}')
+
+
+    # This is a list containing the objects/things that are passed to
+    # the entry_maps functions for populating the table.
     @property
     def entries(self):
         return self._entries
 
 
-    @entries.setter
-    def entries(self, e):
-        self._entries = e
-        for entry in e:
-            for col in range(self.cols):
-                self.add_widget(self._entry_map[col](entry))
+    @property
+    def entry_maps(self):
+        return self._entry_maps
 
 
-    @entry_map.setter
-    def entry_map(self, em):
-        self._entry_map = em
+    @entry_maps.setter
+    def entry_maps(self, em):
+        self._entry_maps = em
         self.cols = len(em)
 
 
 class MainApp(App):
     def build(self):
-        t = Table()
-        return Table()
+        def first(entry):
+            return Label(text=entry['StopAreaName'])
+        def second(entry):
+            return Button(text=entry['TransportMode'], background_color=(200,1,1,1))
+        def third(entry):
+            return Label(text=entry['DisplayTime'])
+        ems = [first, second, third]
+        t = Table(source=lambda: get_departures_from([1525, 1500]), entry_maps=ems)
+        t.update()
+        return t
 
 
 if __name__ == '__main__':
