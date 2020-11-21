0a860b320a0180eeaf5dadc7c9b450903ca25aa5
gustav <gustav@bernpaintner.com>
2020-11-21 16:35:46 +0100

    Swedish weather_data and downloaded weather icons

--- weather_data_source.py
+++ weather_data_source.py
@@ -20,7 +20,10 @@ def request_real_time_forecast(*, site, key, days=3, **additional_params):
 
 
 def get_forecast():
-    forecast = request_real_time_forecast(site='Stockholm', key='06b0d00b98214b5dae880412202111')
+    forecast = request_real_time_forecast(
+            site='59.29687,18.051356',
+            lang='sv',
+            key='06b0d00b98214b5dae880412202111')
     with open("forecast.json", "w", encoding="utf8") as savefile:
         json.dump(forecast, savefile, ensure_ascii=False, indent=4)
     return None
